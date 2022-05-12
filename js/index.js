9fe3c33dfa19c982f61c6941b4bc534fabbff523
stevekelly27 <stevekelly28@hotmail.com>
2022-05-12 16:56:26 +0000

    addding keydown effect in javascript

--- js/index.js
+++ js/index.js
@@ -10,30 +10,28 @@ for (let pad of instrumentPads) {
   
 }
 
+let audio = new Audio('sounds/triangle.mp3', 'sounds/closed-hihat.mp3', 'sounds/open-hihat.mp3')
+
+let sound = document.querySelectorAll('data-url')
+
+let volume = document.querySelector("#volumeslider");
+audio.addEventListener("change", function(e) { console.log(audio.volume)
+audio.volume = (`e.currentTarget.value / 100`);
+});
 
 
+document.addEventListener("keydown", function(event) {
+    instrumentPads.forEach(e1 =>
+        e1.classList.add('active'));
+});
+
+document.addEventListener("keyup", function(event) {
+    instrumentPads.forEach(e1 =>
+        e1.classList.remove('active'));
+});
 
 
-// document.addEventListener("keydown", function(event) {
-//     let active = document.getElementsByClassName('pads');
-//     active.classList.add('active');
-// });
-
-
-
-
-
-
-// let audio = new Audio(['sounds/triangle.mp3', 'sounds/closed-hihat.mp3', 'sounds/open-hihat.mp3']);
-
-// let volume = document.querySelector("#volumeslider");
-// volume.addEventListener("change", function(e) {
-// audio.volume = e.currentTarget.value / 100;
-// });
-
-
-
-document.addEventListener('keydown', (event) => {
+window.addEventListener('keydown', (event) => {
 
     if (event.key === 'r') {
         playSound('sounds/triangle.mp3');
@@ -78,3 +76,4 @@ function playSound(soundUrl) {
     var audio = new Audio(soundUrl);
     audio.play();
 };
+
