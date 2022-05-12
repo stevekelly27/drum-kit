02550d51a1bff1966cbd927ce96fdcd67d731f90
stevekelly27 <stevekelly28@hotmail.com>
2022-05-12 11:12:21 +0000

    Added media queries

--- js/index.js
+++ js/index.js
@@ -1,16 +1,39 @@
 let instrumentPads = document.querySelectorAll('.pads')
 
 for (let pad of instrumentPads) {
-  let soundUrl = pad.dataset.url
+
+  let soundUrl = pad.dataset.url    
 
   pad.addEventListener('click', function(event) {
     playSound(soundUrl)
   })
-
+  
 }
 
 
-window.addEventListener('keydown', (event) => {
+
+
+
+// document.addEventListener("keydown", function(event) {
+//     let active = document.getElementsByClassName('pads');
+//     active.classList.add('active');
+// });
+
+
+
+
+
+
+// let audio = new Audio(['sounds/triangle.mp3', 'sounds/closed-hihat.mp3', 'sounds/open-hihat.mp3']);
+
+// let volume = document.querySelector("#volumeslider");
+// volume.addEventListener("change", function(e) {
+// audio.volume = e.currentTarget.value / 100;
+// });
+
+
+
+document.addEventListener('keydown', (event) => {
 
     if (event.key === 'r') {
         playSound('sounds/triangle.mp3');
@@ -53,5 +76,5 @@ window.addEventListener('keydown', (event) => {
 
 function playSound(soundUrl) {
     var audio = new Audio(soundUrl);
-     audio.play();
+    audio.play();
 };
